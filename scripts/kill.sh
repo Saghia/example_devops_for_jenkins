fdb444c580a33584055f4f301d7d818a1c475a55
Saghia <ing.andreasaggio@gmail.com>
2023-10-09 15:20:25 +0200

    wip

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -4,9 +4,11 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-pid=$(cat .pidfile)
-if [ -n "$pid" ]; then
-    kill "$pid"
-else
-    echo "Process not found or .pidfile is empty."
-fi
+echo '--- ATTENZIONE ---'
+echo $pid
+# pid=$(cat .pidfile)
+# if [ -n "$pid" ]; then
+#     kill "$pid"
+# else
+#     echo "Process not found or .pidfile is empty."
+# fi
