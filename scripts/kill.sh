e65b73764e52d2f2184acd5fbcfdf2e154db1c10
Saghia <ing.andreasaggio@gmail.com>
2023-10-09 15:56:07 +0200

    wip

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -8,7 +8,7 @@ echo $(cat .pidfile)
 echo '--- ATTENZIONE ---'
 echo $pid
 
-ps pid,tty,time | grep -e "npm start" | grep -v "grep" | awk '{print $1}' | kill
+kill $(ps pid,tty,time | grep -e "npm start" | grep -v "grep" | awk '{print $1}')
 ps pid,tty,time | grep -e "npm start"
 
 # pid=$(cat .pidfile)
